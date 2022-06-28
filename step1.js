093596702063a861d0b35b1d948b74c14e1270e6
Alex Linden <alextl2004@gmail.com>
2022-06-28 12:05:04 -0700

    first commit, step one

---
+++ step1.js
@@ -0,0 +1,15 @@
+const fsP = require("fs/promises");
+
+async function cat(path) {
+  try {
+    let contents = await fsP.readFile(path, "utf8");
+    console.log("file contents", contents);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+const argv = process.argv;
+
+cat(argv[2]);
